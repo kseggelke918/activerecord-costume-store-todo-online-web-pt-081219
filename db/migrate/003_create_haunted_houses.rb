7c10efb476ca8cd47534ded446bca2c3e72f9a86
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-31 20:12:20 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,14 @@ class HauntedHouse < ActiveRecord::Migration
 
   def change 
     create_table :haunted_houses do |t|
-      
+      t.string :name 
+      t.string :location 
+      t.string :theme 
+      t.integer :price 
+      t.string :friendly
+      t.datetime :opening_date 
+      t.datetime :closing_date 
+      t.string :long_description
     
   end 
   
