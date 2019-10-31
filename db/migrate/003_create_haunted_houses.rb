365d0b01ae9524a978d2a680e32d3d72dd8cbe12
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-31 20:34:45 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,6 +10,7 @@ class CreateHauntedHouse < ActiveRecord::Migration[4.2]
       t.datetime :opening_date 
       t.datetime :closing_date 
       t.text :long_description
+    end 
     
   end 
   
