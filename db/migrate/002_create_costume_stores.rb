20c4beb188155ba91a8dda61593217a3e9ec8212
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-31 20:50:48 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location 
-      t.integer :number_of_costumes 
-      t.integer :number_of_employees
+      t.integer :costume_inventory 
+      t.integer :employees_count
       t.boolean :in_business
       t.datetime :opening_time
       t.datetime :closing_time
